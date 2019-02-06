9ad5750a3048dbeec591edd248beee3e602a8e2b
Benjamin Schneider <benjamin.schneider.97@gmail.com>
2019-02-06 13:30:24 +0100

    finish first part

--- intemediate exercise/jquery-manipulating-html-document/js/myscript.js
+++ intemediate exercise/jquery-manipulating-html-document/js/myscript.js
@@ -1,24 +1,22 @@
 $(document).ready(function(){
+/*var computer = [];*/
+	for (i=0; i<6; i++){
+		img = $(".product-image img:eq("+i+")").attr("src");
+		notebook = img.substring(4,12);
+		phone = img.substring(4, 9);
+		if (notebook == "notebook"){
+		  $("div.product-image:eq("+i+")").css("background-color", "red");
+			}
+		else if(phone == "phone"){
+		  $("div.product-image:eq("+i+")").css("background-color", "green");
+			}
+		else{
+		  $("div.product-image:eq("+i+")").css("background-color", "blue");
+			}
+		}
 
-// $('div.product-image:nth-child(1),div.product-image:nth-child(2)').attr("name",'notebook').css('background-color', 'red');
-
-var computer = [];
-
-for (i=0; i<6; i++){
-img = $(".product-image img:eq("+i+")").attr("src");
-img = img.substring(4,12);
-if (img == "notebook"){
-  
-  $("div.product-image:eq("+i+")").css("background-color", "red");
-
-
-
-}
-
-
-}
-
-
-
+/*$('div.product-image:eq(0),div.product-image:eq(1)').attr("name",'notebook').css('background-color', 'red');
+$('div.product-image:eq(2),div.product-image:eq(3)').attr("name",'phone').css('background-color', 'green');
+$('div.product-image:eq(4),div.product-image:eq(5)').attr("name",'tablet').css('background-color', 'blue');*/
 
 });
