eddf796ad26cdae9346582a6783a3c6cec97dc6c
Benjamin Schneider <benjamin.schneider.97@gmail.com>
2019-02-06 14:38:55 +0100

    finish advanced

--- advanced exercise/airplane_exercise_files/myscript.js
+++ advanced exercise/airplane_exercise_files/myscript.js
@@ -1,16 +1,11 @@
 $(document).ready(function(){
-
-$(document).mousemove(function(e){
-
-	$('img').css('right', e.pageX+'150px');
-	$('img').css('left', e.pageY + '50px');
-
-
+	$(document).on("mousemove", function(e){
+		$("#moveme").css("position", "absolute");
+		var left = e.pageX -130;
+		var top = e.pageY -115;
+		$('#moveme').css({'left': left, "top": top});
+		});
+	$(document).on("click", function(ev){
+		$("#moveme").fadeOut(500).fadeIn(500).removeAttr("id");
+		});
 });
-
-
-
-});
-
-
-
