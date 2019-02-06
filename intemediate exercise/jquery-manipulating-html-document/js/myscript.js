d26c95fdcc0d3b9940f417f1c3b0ff39180a6cd2
Benjamin Schneider <benjamin.schneider.97@gmail.com>
2019-02-06 13:54:31 +0100

    add third solution for intermediate

--- intemediate exercise/jquery-manipulating-html-document/js/myscript.js
+++ intemediate exercise/jquery-manipulating-html-document/js/myscript.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-/*var computer = [];*/
 	for (i=0; i<8; i++){
 		img = $(".product-image img:eq("+i+")").attr("src");
 		notebook = img.substring(4,12);
@@ -15,8 +14,11 @@ $(document).ready(function(){
 			}
 		}
 
-/*$('div.product-image:eq(0),div.product-image:eq(1)').attr("name",'notebook').css('background-color', 'red');
-$('div.product-image:eq(2),div.product-image:eq(3)').attr("name",'phone').css('background-color', 'green');
-$('div.product-image:eq(4),div.product-image:eq(5)').attr("name",'tablet').css('background-color', 'blue');*/
+	/*$('div.product-image:eq(0),div.product-image:eq(1)').attr("name",'notebook').css('background-color', 'red');
+	$('div.product-image:eq(2),div.product-image:eq(3)').attr("name",'phone').css('background-color', 'green');
+	$('div.product-image:eq(4),div.product-image:eq(5)').attr("name",'tablet').css('background-color', 'blue');*/
 
+	/*$("div.product-image img[src*='phone']").css("background-color", "green");
+	$("div.product-image img[src*='tablet']").css("background-color", "blue");
+	$("div.product-image img[src*='notebook']").css("background-color", "red");*/
 });
